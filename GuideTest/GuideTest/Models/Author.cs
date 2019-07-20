b657edf39f5049a772eac0d26e8d6c1a667f820e
Andre Torres <atcorrea17@gmail.com>
2019-07-20 18:28:22 -0300

    setting up sqlite db

---
+++ GuideTest/GuideTest/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace GuideTest.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        public string NameString { get; set; }
+
+        public string AuthorName { get; set; }
+    }
+}
