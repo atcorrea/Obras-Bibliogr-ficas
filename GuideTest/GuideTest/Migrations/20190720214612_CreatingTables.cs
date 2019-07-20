062c65c68d5779cd12ecc33cd0ed9a6a5aec2bc9
Andre Torres <atcorrea17@gmail.com>
2019-07-20 18:51:14 -0300

    Included sqlite DB and tables

---
+++ GuideTest/GuideTest/Migrations/20190720214612_CreatingTables.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GuideTest.Migrations
+{
+    public partial class CreatingTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NameString = table.Column<string>(nullable: true),
+                    AuthorName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
